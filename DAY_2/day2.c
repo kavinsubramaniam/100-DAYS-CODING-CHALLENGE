fed1cb8eb04b470171adb797216a1ea90b9cde14
kavinsubramaniam <124891339+kavinsubramaniam@users.noreply.github.com>
2023-04-17 22:20:32 +0530

    Update day2.c

--- DAY_2/day2.c
+++ DAY_2/day2.c
@@ -1,4 +1,6 @@
 // Problem
+// Easy Pronunciation
+
 // Words that contain many consecutive consonants, like "schtschurowskia", are generally considered somewhat hard to pronounce.
 // We say that a word is hard to pronounce if it contains 4 or more consonants in a row; otherwise it is easy to pronounce. For example, "apple" and "polish" are easy to pronounce, but "schtschurowskia" is hard to pronounce.
 // You are given a string S consisting of N lowercase Latin characters. Determine whether it is easy to pronounce or not based on the rule above — print YES if it is easy to pronounce and NO otherwise.
